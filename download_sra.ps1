bcdde19c5ecc19815fa59b615a938a2694e5ea03
Kaede Ito <mapleia01@gmail.com>
2023-11-22 03:25:43 -0800

    fixed powershell script

--- download_sra.ps1
+++ download_sra.ps1
@@ -1,1 +1,6 @@
-prefetch --option-file "./references/sra_id_list_test.txt" --output-directory "sra_files"
+prefetch --option-file "./references/srr_id_list.txt" --output-directory "sra_files"
+[string[]]$arrayFromFile = Get-Content -Path './references/srr_id_list.txt'
+
+for ($srr_id in $arrayFromFile) {
+    fasterq-dump --outdir "./sra_files" $srr_id
+}
